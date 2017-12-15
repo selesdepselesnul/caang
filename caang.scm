4d273df3e9e248b0677585ae9ef822e58f9f7dea
selesdepselesnul <selesdepselesnul@gmail.com>
2017-12-16 06:17:46 +0700

    read brigthness from arg

--- caang.scm
+++ caang.scm
@@ -1,3 +1,13 @@
 #!/usr/bin/env ./runscheme.sh
-(print "init")
+;; -*- geiser-scheme-implementation: 'chicken -*-
+(define backlight-brigthness "/sys/class/backlight/intel_backlight/brightness")
+
+(define (read-param)
+  (car (command-line-arguments)))
+
+(with-output-to-file
+    backlight-brigthness
+  (lambda ()
+    (format #t (read-param) 1)))
+
 
